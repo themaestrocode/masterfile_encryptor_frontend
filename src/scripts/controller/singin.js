99d127c5ae7cc7e5282c9a927f8c2b6e6877f970
victor <victorsoderu@gmail.com>
2024-03-14 21:13:57 +0100

    new commit

--- src/scripts/controller/singin.js
+++ src/scripts/controller/singin.js
@@ -1,10 +1,11 @@
 const signinSection = document.querySelector(".js-signin-form-section");
+const signinAttemptMessage = document.querySelector(".js-signin-attempt-message");
 
 renderLoginFormHTML();
 
 function renderLoginFormHTML() {
    signinSection.innerHTML = `
-      <form class="signin-form js-signin-form">
+      <form class="login-form js-login-form">
          <label for="email">Email</label><br>
          <input id="email" class="email js-email" type="email" placeholder="Enter your email address"><br>
 
@@ -24,7 +25,7 @@ function renderLoginFormHTML() {
 
 function renderSignupFormHTML() {
    signinSection.innerHTML = `
-      <form class="signin-form js-signin-form">
+      <form class="signup-form js-signup-form">
          <label for="email">Email</label><br>
          <input id="email" class="email js-email" type="email" placeholder="Enter your email address"><br>
 
@@ -43,4 +44,45 @@ function renderSignupFormHTML() {
    `;
 
    document.querySelector(".js-login-suggestion").addEventListener("click", () => renderLoginFormHTML());
+
+   document.querySelector(".js-signup-form").addEventListener("submit", (event) => {
+      event.preventDefault();
+      
+      const email = document.querySelector(".js-email");
+      const password = document.querySelector(".js-password");
+      const confirmPassword = document.querySelector(".js-confirm-password");
+
+      if (!validateUserRegistrationData(email.value, password.value, confirmPassword.value)) return;
+
+      const userDetailsObject = { email : email.value, password : password.value };
+      registerUser(userDetailsObject);
+   });
+}
+
+function validateUserRegistrationData (email, password, confirmPassword) {
+   if (email.length === 0) {
+      displaySigninAttemptFailedMessage("invalid email address!");
+      return false;
+   } else if (password.trim().length === 0 || password.length < 8) {
+      displaySigninAttemptFailedMessage("Password must be at least 8 characters longs.");
+      return false;
+   } else if (password !== confirmPassword) {
+      displaySigninAttemptFailedMessage("Passwords do not match. Check and try again.");
+      return false;
+   }
+
+   displaySigninAttemptSuccessMessage("success");
+   return true;
+}
+
+function registerUser(userDetailsObject) {
+   console.log(userDetailsObject);
+}
+
+function displaySigninAttemptFailedMessage (message) {
+   signinAttemptMessage.innerHTML = `<p class="error-message">${message}</p>`;
+}
+
+function displaySigninAttemptSuccessMessage (message) {
+   signinAttemptMessage.innerHTML = `<p class="success-message"><img src="icons/checkmark.png">  ${message}</p>`;
 }
